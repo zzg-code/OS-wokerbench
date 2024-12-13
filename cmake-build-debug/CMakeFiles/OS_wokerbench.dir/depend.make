04e361af4d9ce17e5faf44f1daf2a1b9617bc66b
zzg <2421380682@qq.com>
2024-12-13 16:04:27 +0800

    Stop tracking cmake-build-debug and .idea directories

--- cmake-build-debug/CMakeFiles/OS_wokerbench.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for OS_wokerbench.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/OS_wokerbench.dir/src/libco/co.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/OS_wokerbench.dir/src/libco/co.cpp.o: \
- /tmp/tmp.d1TliMJTPL/src/libco/co.cpp /usr/include/stdc-predef.h \
- /tmp/tmp.d1TliMJTPL/src/libco/co.h
